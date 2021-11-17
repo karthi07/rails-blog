ad36c79bda66c7a8e74fc964a43542b67a1d575d
Karthick Venkatesan <itkarthi02@gmail.com>
2021-11-17 22:40:10 +0530

    add update and destory

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
 
@@ -19,6 +19,27 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
   end
 
   private
